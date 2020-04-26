502d0316c8250e55072727e592804535951ec590
Robin Chang <sumdumghai@gmail.com>
2020-04-26 21:05:48 +0800

    Change MedPod to 3x3 cell building
    This allows the MedPod to be more easily and intuitively selectable, with the trade-off of requiring Harmony patching to force the (nominally 3-pawn) 3x3 cell bed to act like a single 1x2 cell bed.

---
+++ Source/MedPodDef.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MedPod
+{
+    [DefOf]
+    public static class MedPodDef
+    {
+        public static ThingDef MedPodStandard;
+    }
+}
--- Source/MedPodMod.cs
+++ Source/MedPodMod.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace MedPod
@@ -10,6 +12,8 @@ namespace MedPod
         public MedPodMod(ModContentPack content) : base(content)
         {
             Settings = GetSettings<Settings>();
+            var harmony = new Harmony("com.MedPod.patches");
+            harmony.PatchAll();
         }
 
         public override void DoSettingsWindowContents(Rect canvas)
@@ -22,5 +26,32 @@ namespace MedPod
         {
             return "MedPod_SettingsCategory_Heading".Translate();
         }
+
+        // Manually force the MedPod beds to only have one sleeping slot located in the center cell of the nominally 3x3 furniture, as by default RimWorld will assume a 3x3 "bed" should have three slots positioned in the top row cells
+
+        [HarmonyPatch(typeof(Building_Bed), nameof(Building_BedMedPod.SleepingSlotsCount), MethodType.Getter)]
+        static class BuildingBed_SleepingSlotsCount
+        {
+            static void Postfix(ref int __result, ref Building_Bed __instance)
+            {
+                if (__instance.def == MedPodDef.MedPodStandard)
+                {
+                    __result = 1;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Building_Bed), nameof(Building_BedMedPod.GetSleepingSlotPos))]
+        static class BuildingBed_GetSleepingSlotPos
+        {
+            static void Postfix(ref IntVec3 __result, ref Building_Bed __instance, int index)
+            {
+                if (__instance.def == MedPodDef.MedPodStandard)
+                {
+                    __result = __instance.Position;
+                }
+            }
+        }
+
     }
 }
