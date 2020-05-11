b4c38cd242c14cbc9355a38ad3dc7a7b679afca1
Robin Chang <sumdumghai@gmail.com>
2020-05-11 21:38:40 +0800

    Add MedPod-specific StatDefs to info window display

--- Source/CompProperties_MedPodSettings.cs
+++ Source/CompProperties_MedPodSettings.cs
@@ -1,4 +1,7 @@
-﻿using Verse;
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Verse;
 
 namespace MedPod
 {
@@ -17,6 +20,19 @@ namespace MedPod
         public CompProperties_MedPodSettings()
         {
             compClass = typeof(CompMedPodSettings);
+        }
+
+        [DebuggerHidden]
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(StatRequest req)
+        {
+            foreach (StatDrawEntry s in base.SpecialDisplayStats(req))
+            {
+                yield return s;
+            }
+            yield return new StatDrawEntry(StatCategoryDefOf.Building, "MedPod_Stat_PowerConsumptionDiagnosis_Label".Translate(), diagnosisModePowerConsumption.ToString("F0") + " W", "MedPod_Stat_PowerConsumptionDiagnosis_Desc".Translate(), 4994);
+            yield return new StatDrawEntry(StatCategoryDefOf.Building, "MedPod_Stat_PowerConsumptionHealing_Label".Translate(), healingModePowerConsumption.ToString("F0") + " W", "MedPod_Stat_PowerConsumptionHealing_Desc".Translate(), 4993);
+            yield return new StatDrawEntry(StatCategoryDefOf.Building, "MedPod_Stat_DiagnosisTime_Label".Translate(), "MedPod_Stat_TimeSeconds".Translate(maxDiagnosisTime), "MedPod_Stat_DiagnosisTime_Desc".Translate(), 4992);
+            yield return new StatDrawEntry(StatCategoryDefOf.Building, "MedPod_Stat_PerHediffHealingTime_Label".Translate(), "MedPod_Stat_TimeSeconds".Translate(maxPerHediffHealingTime), "MedPod_Stat_PerHediffHealingTime_Desc".Translate(), 4991);
         }
     }
 }
