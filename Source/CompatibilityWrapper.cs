e9757db261256832eb6561efafa7fd6b654ed3a4
Robin Chang <sumdumghai@gmail.com>
2020-08-22 18:51:36 +0800

    Fix compatibility issue with Elite Bionics Framework
    For #22

---
+++ Source/CompatibilityWrapper.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace MedPod
+{
+    // Wrapper class for compatibility with mods like Elite Bionics Framework
+
+    public class CompatibilityWrapper
+    {
+        public static MethodInfo makeshiftEbfEndpoint = AccessTools.Method("EBF.VanillaExtender:GetMaxHealth");
+
+        public static FastInvokeHandler myDelegate = null;
+
+        public static float GetMaxHealth(BodyPartDef def, Pawn pawn, BodyPartRecord record)
+        {
+            if (makeshiftEbfEndpoint != null)
+            {
+                if (myDelegate == null)
+                {
+                    myDelegate = MethodInvoker.GetHandler(makeshiftEbfEndpoint);
+                }    
+                return (float)myDelegate(null, new object[] { def, pawn, record });
+            }
+            return def.GetMaxHealth(pawn);
+        }
+    }    
+}
