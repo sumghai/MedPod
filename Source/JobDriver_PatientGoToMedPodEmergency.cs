a98dfcf9d1024d171bb31dd82376d6aaa8db735e
sumghai <sumdumghai@gmail.com>
2021-08-17 21:44:15 +0800

    Clean up unneeded debug log messages
    Fpr #47

--- Source/JobDriver_PatientGoToMedPodEmergency.cs
+++ Source/JobDriver_PatientGoToMedPodEmergency.cs
@@ -23,7 +23,6 @@ namespace MedPod
 
         public override IEnumerable<Toil> MakeNewToils()
         {
-            Log.Warning("MedPod :: JobDriver_PatientGoToMedPodEmergency.MakeNewToils() - Making new toils for " + BedMedPod.ToString() + " ("+ BedMedPod.Label.ToString() + ")");
             this.FailOnDespawnedNullOrForbidden(MedPodInd);
             this.FailOnBurningImmobile(MedPodInd);
             this.FailOn(delegate
@@ -35,9 +34,7 @@ namespace MedPod
             {
                 job.count = 1;
             });
-            Log.Warning("\tReturning Toil for going to bed (target=" + BedMedPod.ToString() + ")");
             yield return Toils_Bed.GotoBed(MedPodInd);
-            Log.Warning("\tReturning Toil for laying down (target=" + BedMedPod.ToString() + ")");
             yield return Toils_LayDown.LayDown(MedPodInd, true, lookForOtherJobs: false);
         }
     }
