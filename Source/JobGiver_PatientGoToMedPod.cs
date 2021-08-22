8d4cc5f90556e58e9dc639b7203f0f88ce82d727
sumghai <sumdumghai@gmail.com>
2021-08-22 21:50:55 +0800

    Allow ambulatory prisoner patients to use MedPods
    For #47

---
+++ Source/JobGiver_PatientGoToMedPod.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace MedPod
+{
+    public class JobGiver_PatientGoToMedPod : ThinkNode
+    {
+        public override ThinkResult TryIssueJobPackage(Pawn pawn, JobIssueParams jobParams)
+        {
+            Building_BedMedPod bedMedPod = WorkGiver_DoctorRescueToMedPod.FindBestMedPod(pawn, pawn);
+            if (bedMedPod != null)
+            {
+                return new ThinkResult(JobMaker.MakeJob(MedPodDef.PatientGoToMedPod, bedMedPod), this);
+            }
+                        
+            return ThinkResult.NoJob;
+        }
+    }
+}
