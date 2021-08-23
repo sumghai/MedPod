230dc1f77bdd1b3bfe830c722eff63133b7bfb88
sumghai <sumdumghai@gmail.com>
2021-08-23 21:34:05 +0800

    Change patient rest jobs to use existing JobGiver
    For #47

--- Source/WorkGiver_PatientGoToMedPod.cs
+++ Source/WorkGiver_PatientGoToMedPod.cs
@@ -1,45 +1,21 @@
 ﻿using RimWorld;
-using System.Collections.Generic;
 using Verse;
 using Verse.AI;
 
 namespace MedPod
 {
-    public class WorkGiver_PatientGoToMedPod : WorkGiver_Scanner
+    public class WorkGiver_PatientGoToMedPod : WorkGiver
     {
-        public static string NoPathTrans;
+        public static JobGiver_PatientGoToMedPod jgpgtmp = new JobGiver_PatientGoToMedPod();
 
-        public override bool Prioritized => true;
-
-        public override PathEndMode PathEndMode => PathEndMode.Touch;
-
-        public static void ResetStaticData()
+        public override Job NonScanJob(Pawn pawn)
         {
-            NoPathTrans = "NoPath".Translate();
-        }
-
-        public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
-        {
-            return pawn.Map.listerBuildings.AllBuildingsColonistOfClass<Building_BedMedPod>();
-        }
-
-        public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
-        {
-            Building_BedMedPod bedMedPod = t as Building_BedMedPod;
-            return MedPodRestUtility.IsValidMedPodFor(bedMedPod, pawn, pawn, pawn.GuestStatus);
-        }
-
-        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
-        {
-            return JobMaker.MakeJob(MedPodDef.PatientGoToMedPod, t);
-        }
-
-        public override float GetPriority(Pawn pawn, TargetInfo t)
-        {
-            // TODO - include distance in heuristics?
-            float value = t.Thing.def.GetStatValueAbstract(StatDefOf.BedRestEffectiveness);
-            Log.Warning(t.Thing.ToString() + " (" + t.Thing.Label.ToString() + ") has a priority value of " + value.ToString());
-            return value;
+            ThinkResult thinkResult = jgpgtmp.TryIssueJobPackage(pawn, default(JobIssueParams));
+            if (thinkResult.IsValid)
+            {
+                return thinkResult.Job;
+            }
+            return null;
         }
     }
 }
