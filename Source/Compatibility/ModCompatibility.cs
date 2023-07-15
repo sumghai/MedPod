034bf9ef650075d908d06e241cbc28d390943682
sumghai <sumghai@users.noreply.github.com>
2023-07-15 12:29:21 +1200

    Tweak Alpha Genes compatibility patch
    This ensures the patch only runs when the main Alpha Genes mod is present, and not accidentally also triggered by the independent Alpha Genes - Insectoid Mutations gene pack

--- Source/Compatibility/ModCompatibility.cs
+++ Source/Compatibility/ModCompatibility.cs
@@ -5,8 +5,8 @@ namespace MedPod
 {
     public class ModCompatibility
     {
-        // Alpha Genes
-        public static bool AlphaGenesIsActive => ModLister.AllInstalledMods.Where(x => x.Active && x.PackageId.Contains("sarg.alphagenes".ToLower())).Any();
+        // Alpha Genes (use PackageId.Equals to match only the main Alpha Genes mod, and not the Alpha Genes - Insectoid Mutations gene pack)
+        public static bool AlphaGenesIsActive => ModLister.AllInstalledMods.Where(x => x.Active && x.PackageId.Equals("sarg.alphagenes".ToLower())).Any();
 
         // Android Tiers
         public static bool AndroidTiersIsActive => ModLister.AllInstalledMods.Where(x => x.Active && x.PackageId.Contains("Atlas.AndroidTiers".ToLower())).Any();
